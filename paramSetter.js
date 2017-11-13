a05e2f2e2907a133ebb51e2cb2c4c601481d5948
Yohan R. Desanti García <ydesanti@llbean.com>
2017-11-13 10:27:29 -0600

    Update paramSetter.js

--- paramSetter.js
+++ paramSetter.js
@@ -9,13 +9,15 @@ link.media = 'all';
 head.appendChild(link);
 
 var divModal = document.createElement("div");
+var fragment = document.createDocumentFragment();
 divModal.innerHTML = '<div id="myModal" class="modal"><div class="modal-content"><span class="close">&times;</span><p>Select the desired parameters to add</p>'+
                   '<form action="/action_page.php"><input type="checkbox" value="fea">FEA<br><input type="checkbox" value="pla">PLA<br><input type="checkbox" value="stopmobi">StopMobi<br>'+
                   '<input type="checkbox" value="promocode">PromoCode <input type="text" id="pcd" size="50"><br><br><input type="button" onclick="urlUpdate()" value="Send order">'+
                   '</form></div></div>';
+fragment.appendChild(divModal);
 
 // Get the modal
-var modal = document.getElementById('myModal');
+//var modal = document.getElementById('myModal');
 // Get the button that opens the modal
 //var btn = document.getElementById("myBtn");
 // Get the <span> element that closes the modal
@@ -36,35 +38,35 @@ var span = document.getElementsByClassName("close")[0];
 }*/
 
 function injectForm() {
-	var doc = document.getElementById("content-anchor");
-	doc.replaceWith(divModal);
+  var doc = document.getElementById("content-anchor");
+  doc.replaceWith(divModal);
 }
 
 function urlUpdate() {
-	var url = document.URL;	
+  var url = document.URL; 
     var checks = document.forms[0];
     
-	if(checks[0].checked){
-		url += separador(url) + 'featest=1';
-	} 
+  if(checks[0].checked){
+    url += separador(url) + 'featest=1';
+  } 
     
     if (checks[1].checked){
-		url += separador(url) + 'PLA1=0';
-	}
+    url += separador(url) + 'PLA1=0';
+  }
     
     if (checks[2].checked){
-		url += separador(url) + 'stop_mobi=yes';
-	}
+    url += separador(url) + 'stop_mobi=yes';
+  }
     
     if (checks[3].checked){
-		url += separador(url) + 'pcd=' +checks[4].value;
-	}
+    url += separador(url) + 'pcd=' +checks[4].value;
+  }
     
-	window.location = url; alert(url);
+  window.locaation = url; alert(url);
 }
 
 function separador(url) {
-	var n = url.indexOf("?");
+  var n = url.indexOf("?");
     var separador = n>0 ? "&" : "?";
     return separador;
 }
