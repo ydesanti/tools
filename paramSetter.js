4dff755eff7696277bd14220445337ce7918a4ce
Yohan R. Desanti García <ydesanti@llbean.com>
2017-11-13 11:08:12 -0600

    Update paramSetter.js

--- paramSetter.js
+++ paramSetter.js
@@ -17,56 +17,48 @@ divModal.innerHTML = '<div id="myModal" class="modal"><div class="modal-content"
 fragment.appendChild(divModal);
 
 // Get the modal
-//var modal = document.getElementById('myModal');
-// Get the button that opens the modal
-//var btn = document.getElementById("myBtn");
+var modal = document.getElementById('myModal');
+
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
-// When the user clicks the button, open the modal 
-//btn.onclick = function() {
-//    modal.style.display = "block";
-//}
+
 // When the user clicks on <span> (x), close the modal
-/*span.onclick = function() {
+span.onclick = function() {
     modal.style.display = "none";
-}*/
+}
 // When the user clicks anywhere outside of the modal, close it
-/*window.onclick = function(event) {
+window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
-}*/
-
-function injectForm() {
-  var doc = document.getElementById("content-anchor");
-  doc.replaceWith(divModal);
 }
 
 function urlUpdate() {
   var url = document.URL; 
-    var checks = document.forms[0];
+  var checks = document.forms[0];
     
   if(checks[0].checked){
     url += separador(url) + 'featest=1';
   } 
     
-    if (checks[1].checked){
+  if (checks[1].checked){
     url += separador(url) + 'PLA1=0';
   }
     
-    if (checks[2].checked){
+  if (checks[2].checked){
     url += separador(url) + 'stop_mobi=yes';
   }
     
-    if (checks[3].checked){
+  if (checks[3].checked){
     url += separador(url) + 'pcd=' +checks[4].value;
   }
     
-  window.locaation = url; alert(url);
+  window.locaation = url;
+  console(url);
 }
 
 function separador(url) {
   var n = url.indexOf("?");
-    var separador = n>0 ? "&" : "?";
+  var separador = n>0 ? "&" : "?";
     return separador;
 }
